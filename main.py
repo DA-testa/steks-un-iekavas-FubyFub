a96739ab3e50a3a5cd4fccb6a4ee3589254b9bb3
Andrejs Vasiljevs 221RDB441 <125797971+FubyFub@users.noreply.github.com>
2023-02-19 14:46:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def find_mismatch(text):
                 return i + 1
     if opening_brackets_stack:
         return opening_brackets_stack[0].position
-    return  "Success"
+    return "Success"
 
 
 def main():
