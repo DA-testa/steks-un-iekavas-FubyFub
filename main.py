957ae16c2611f7caed512e9b62e9ee5632e65491
Andrejs Vasiljevs 221RDB441 <125797971+FubyFub@users.noreply.github.com>
2023-02-19 15:35:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,17 @@ def are_matching(left, right):
 def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
-        if next not in "I\r\n":
-            if next in "([{":
-                opening_brackets_stack.append( Bracket( next, i + 1 ))
-                pass
+        if next in "([{":
+            opening_brackets_stack.append( Bracket( next, i + 1 ))
+            pass
 
-            if next in ")]}":
-                if not opening_brackets_stack:
-                    return i + 1
-                pass
-                high = opening_brackets_stack.pop()
-                if not are_matching( high.char, next ):
-                    return i + 1
+        if next in ")]}":
+            if not opening_brackets_stack:
+                return i + 1
+            pass
+            high = opening_brackets_stack.pop()
+            if not are_matching( high.char, next ):
+                return i + 1
     if opening_brackets_stack:
         return opening_brackets_stack[0].position
     return  "Success"
